e210030055f790dd0372976f362f819dc67d622e
Samuel-S-N <samuelsneres@gmail.com>
2026-03-03 00:49:32 -0300

    aula 03 completa

--- aula_03/atividade_05.py
+++ aula_03/atividade_05.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier,export_text
 from sklearn.metrics import classification_report
 
 # DICA 1: Use pd.read_csv para trazer o arquivo 'dados_intencoes.csv'
@@ -12,5 +12,14 @@ y = df['label']
 
 # --- INÍCIO DO TRABALHO DO ALUNO ---
 # PASSO 1: Divida os dados em treino e teste (use test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 # PASSO 2: Instancie o DecisionTreeClassifier e treine com .fit()
+clf = DecisionTreeClassifier(max_depth=3)
+clf.fit(X_train, y_train)
+
 # PASSO 3: Gere predições e use classification_report(y_test, predicoes) para ver o resultado por classe.
+previsao = clf.predict(X_test)
+
+print("Relatório classificações")
+print(classification_report(y_test, previsao))
